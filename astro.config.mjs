21e50fbf1e857e00cbf785c1139a23a5bb0e4659
Hollis Roberts <hollisaroberts@hotmail.com>
2026-07-06 08:26:10 +0100

    modified:   src/layouts/BaseLayout.astro new file:   public/robots.txt modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@ import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import remarkGfm from 'remark-gfm';
+import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+   site: 'https://temporaldecoupling.com',
   output: 'static',
   markdown: {
     remarkPlugins: [remarkMath, remarkGfm],
     rehypePlugins: [rehypeKatex],
   },
+  integrations: [
+    netlify(),
+    sitemap(),
+  ]
 });
